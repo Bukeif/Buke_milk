3831aa8838c2eeac1647048904d50911feba6a09
buke <az4659811@gmail.com>
2026-05-14 22:23:03 +0800

    add obs time kit

---
+++ OBS/JS/Buke_time.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () =>
+{
+    const clockEl = document.getElementById("clock");
+    const dateEl = document.getElementById("date");
+
+    const weekdays = [
+        "星期日",
+        "星期一",
+        "星期二",
+        "星期三",
+        "星期四",
+        "星期五",
+        "星期六"
+    ]
+
+    function updateClock()
+    {
+        const now = new Date();
+        // clock
+        const timeString = new Intl.DateTimeFormat("zh-TW", {
+            timeZone: "Asia/Taipei",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: false
+        }).format(now);
+
+        //date
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const day = String(now.getDate()).padStart(2, "0");
+        const weekday = weekdays[now.getDay()];
+
+        clockEl.textContent = timeString;
+        dateEl.textContent = `${year}-${month}-${day} ${weekday}`;
+    }
+
+    updateClock();
+    setInterval(updateClock, 1000);
+})
